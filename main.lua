8a20882e6c2849e51ffe197d6753b50f09e9e091
KDXVS <00cvlt00@gmail.com>
2025-04-16 22:32:19 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/KDXVS/gui/refs/heads/main/gay.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KDXVS/gui/refs/heads/main/obf_ah70pp83tzsx1W0U8w7CbCxs7hF9XHSc2Uniuq6H1am61HNKDl9Ds500BZ9fe6V3.lua"))()
